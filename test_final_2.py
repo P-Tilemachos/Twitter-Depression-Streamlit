9018898f0d85281393ac7c0f396db22287a37173
P-Tilemachos <tilemahos00@gmail.com>
2026-01-24 05:12:03 +0200

    Update test_final_2.py

--- test_final_2.py
+++ test_final_2.py
@@ -404,7 +404,6 @@ except Exception:
 # ---------- Load classic ML ----------
 tfidf_vectorizer = joblib.load("tfidf_vectorizer_smote.pkl")
 models = {
-    "Random Forest": joblib.load("rf_model_smote.pkl"),
     "XGBoost": joblib.load("xgb_model_smote.pkl"),
     "Logistic Regression": joblib.load("lr_model_smote.pkl"),
     "SVM": joblib.load("svm_model_smote.pkl"),
@@ -859,3 +858,4 @@ elif is_admin:
         st.caption(info_msg)
     else:
         st.info("Δεν βρέθηκαν λέξεις για 3D απεικόνιση (άδειο ή πολύ μικρό κείμενο).")
+
